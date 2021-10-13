59cfe93e490aa9f8c2cfe7007272d703276953ab
tigasbuto <55861530+tigasbuto@users.noreply.github.com>
2021-10-12 17:50:54 -0700

    Test spoontacular call

---
+++ app/src/main/java/com/example/a491bproject/IngredientInfo.kt
@@ -0,0 +1,58 @@
+package com.example.a491bproject
+
+data class IngredientInfo(
+    val aisle: String,
+    val amount: Double,
+    val categoryPath: List<String>,
+    val consistency: String,
+    val estimatedCost: EstimatedCost,
+    val id: Int,
+    val image: String,
+    val meta: List<Any>,
+    val name: String,
+    val nameClean: String,
+    val nutrition: Nutrition,
+    val original: String,
+    val originalName: String,
+    val possibleUnits: List<String>,
+    val shoppingListUnits: List<String>,
+    val unit: String,
+    val unitLong: String,
+    val unitShort: String
+) {
+    data class EstimatedCost(
+        val unit: String,
+        val value: Double
+    )
+
+    data class Nutrition(
+        val caloricBreakdown: CaloricBreakdown,
+        val nutrients: List<Nutrient>,
+        val properties: List<Property>,
+        val weightPerServing: WeightPerServing
+    ) {
+        data class CaloricBreakdown(
+            val percentCarbs: Double,
+            val percentFat: Double,
+            val percentProtein: Double
+        )
+
+        data class Nutrient(
+            val amount: Double,
+            val name: String,
+            val percentOfDailyNeeds: Double,
+            val unit: String
+        )
+
+        data class Property(
+            val amount: Double,
+            val name: String,
+            val unit: String
+        )
+
+        data class WeightPerServing(
+            val amount: Int,
+            val unit: String
+        )
+    }
+}
--- app/src/main/java/com/example/a491bproject/MainActivity.kt
+++ app/src/main/java/com/example/a491bproject/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.a491bproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,5 +31,36 @@ class MainActivity : AppCompatActivity() {
         if(actionBar != null){
             actionBar.title = "Main Menu"
         }
+
+
+        // Spoontacular call
+        var rf = Retrofit.Builder()
+            .baseUrl(RetrofitInterface.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+
+        var API = rf.create(RetrofitInterface::class.java)
+        var call = API.info
+
+        call?.enqueue(object: Callback<List<IngredientInfo?>?> {
+            override fun onResponse(
+                call: Call<List<IngredientInfo?>?>,
+                response: Response<List<IngredientInfo?>?>
+            ) {
+                var infolist : List<IngredientInfo>? = response.body() as List<IngredientInfo>
+                var info = arrayOfNulls<String>(infolist!!.size)
+
+                for (i in infolist!!.indices)
+                    info[i] = infolist!![i]!!.original
+
+                var adapter = ArrayAdapter<String>(applicationContext, android.R.layout.simple_dropdown_item_1line, info)
+                findViewById<ListView>(R.id.listview)?.adapter = adapter
+
+            }
+
+            override fun onFailure(call: Call<List<IngredientInfo?>?>, t: Throwable) {
+            }
+
+        })
+
     }
 }
