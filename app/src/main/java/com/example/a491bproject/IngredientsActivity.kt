2deb2aff3f95bae9d6b70c42320a74af8938323d
toanos <toan.ha1896@gmail.com>
2021-10-13 13:18:47 -0700

    IngredientsActivity: implement an internal file storage but it is not working.

--- app/src/main/java/com/example/a491bproject/IngredientsActivity.kt
+++ app/src/main/java/com/example/a491bproject/IngredientsActivity.kt
@@ -1,10 +1,15 @@
 package com.example.a491bproject
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
 
 class IngredientsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +36,19 @@ class IngredientsActivity : AppCompatActivity() {
                     quantity.text.toString().toDouble()
                 )
 //                db.insertData(user)
+
+                /* Write to an internal text file */
+                val ingredientsFile : FileOutputStream
+                val contents = ingredient.text.toString() + " , " + quantity.text.toString()
+                Toast.makeText(this, "Adding $contents", Toast.LENGTH_SHORT).show()
+                Log.d("IngredientActivity", "contents: $contents")
+                try {
+                    Log.d("IngredientsActivity", "attempt to create a text file")
+                    ingredientsFile = openFileOutput("ingredients.txt", Context.MODE_PRIVATE)
+                    ingredientsFile.write(contents.toByteArray())
+                } catch (e : FileNotFoundException) {
+                    e.printStackTrace()
+                }
                 clearField()
             } else {
                 Toast.makeText(this, "Input Ingredient and Quantity", Toast.LENGTH_SHORT).show()
