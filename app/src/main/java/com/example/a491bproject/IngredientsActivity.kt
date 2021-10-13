c8857d7d24f82910b70176350cfc88ebd566531f
toanos <toan.ha1896@gmail.com>
2021-10-13 11:04:59 -0700

    IngredientsActivity.kt: copy button click functionality from AddIngredientsActivity.

--- app/src/main/java/com/example/a491bproject/IngredientsActivity.kt
+++ app/src/main/java/com/example/a491bproject/IngredientsActivity.kt
@@ -2,6 +2,9 @@ package com.example.a491bproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class IngredientsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +14,32 @@ class IngredientsActivity : AppCompatActivity() {
         //Naming for menu
         val actionBar = supportActionBar
 
-        if(actionBar != null){
+        if (actionBar != null) {
             actionBar.title = "Ingredients Activity"
         }
+
+        val button = findViewById<TextView>(R.id.buttonAddIngredient)
+        button.setOnClickListener {
+            val db = DataBaseHandler(this)
+            val ingredient: TextView = findViewById(R.id.editTextIngredient)
+            val quantity: TextView = findViewById(R.id.editTextQuantity)
+            if (ingredient.text.toString().isNotEmpty() &&
+                quantity.text.toString().isNotEmpty()
+            ) {
+                val user = AddIngredientsActivity.User(
+                    ingredient.text.toString(),
+                    quantity.text.toString().toDouble()
+                )
+                db.insertData(user)
+                clearField()
+            } else {
+                Toast.makeText(this, "Input Ingredient and Quantity", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun clearField() {
+        findViewById<EditText>(R.id.editTextIngredient).text.clear()
+        findViewById<EditText>(R.id.editTextQuantity).setText("")
     }
 }
