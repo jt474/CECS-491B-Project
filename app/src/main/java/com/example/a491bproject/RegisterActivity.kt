4d5085b7acb3b3b4e0e542da8a5727ccf5b0e2d1
jt474 <49823439+jt474@users.noreply.github.com>
2021-10-25 13:48:25 -0700

    Added navigation to main menu after register
    Sends userID with intent

--- app/src/main/java/com/example/a491bproject/RegisterActivity.kt
+++ app/src/main/java/com/example/a491bproject/RegisterActivity.kt
@@ -63,9 +63,14 @@ class RegisterActivity : AppCompatActivity() {
                                     "You have Registered",
                                     Toast.LENGTH_SHORT
                                 ).show()
-
                                 //New user should be made at this point
 
+                                //Navigates newly made account to main and clear previous intents
+                                val registeredIntent = Intent(this@RegisterActivity, MainActivity::class.java)
+                                registeredIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("userID", firebaseUser.uid)
+                                startActivity(registeredIntent)
+                                finish()
                             }
                             //When Register fails
                             else {
