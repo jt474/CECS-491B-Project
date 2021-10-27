28bd481f6bc6355014817031d666859a5044a84d
Jace <jacen131@gmail.com>
2021-10-27 06:53:40 -0700

    Initial Commit
    need to test the insert functionality.

---
+++ app/src/main/java/com/example/a491bproject/DBHandlers/FirebaseRecipeDAO.kt
@@ -0,0 +1,93 @@
+package com.example.a491bproject.DBHandlers
+
+import android.widget.Toast
+import com.example.a491bproject.MainActivity
+import com.example.a491bproject.models.RecipeModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.lang.NullPointerException
+
+
+class FirebaseRecipeDAO(val auth: FirebaseAuth): RecipeDAO {
+    private val dbRef:DatabaseReference
+    private val userID: String?
+    private val recipeParentStr= "Recipes"
+    private val instructionParentStr = "Instructions"
+
+    init{
+        val user: FirebaseUser? = auth.currentUser;
+        userID = user?.uid;
+        dbRef = FirebaseDatabase.getInstance().reference;
+
+    }
+
+
+    override fun insertRecipe(model:RecipeModel): Int {
+        val recipePush:DatabaseReference = dbRef.child(recipeParentStr).push()
+        val recipePushID = recipePush.key!! //is null if the reference was to a root... but that shouldn't happen.
+
+        val recipeMap = buildRecipeMap(model, recipePushID)
+        val ingredientMap = buildIngredientsMap(model.ingredients)
+
+        var recipeNodeMap: HashMap<String, Any> = hashMapOf(
+            "$recipeParentStr/$recipePushID" to recipeMap
+        )
+        var ingredientNodeMap: HashMap<String, Any> = hashMapOf(
+            "$recipeParentStr/$recipePushID/Ingredients" to ingredientMap
+        )
+        updateChildren(recipeNodeMap)
+        updateChildren(ingredientNodeMap)
+        return 1 //Hack. turns out that with so many children being added... i cann't approach this the sql way.
+    }
+
+    override fun getRecipe(id: String): RecipeModel {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateRecipe(id: String): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteRecipe(id: String): Int {
+        TODO("Not yet implemented")
+    }
+
+    fun buildRecipeMap(model:RecipeModel, recipeId:String): Map<String, String>{
+
+        val map: HashMap<String, String> = hashMapOf(
+            "recipeId" to recipeId,
+            "authorId" to model.authorID,
+            "title" to model.title
+        )
+        return map
+    }
+
+    fun buildIngredientsMap(ingredients: List<String>): Map<String, String>{
+        val map = hashMapOf<String, String>()
+        for(item in ingredients){
+            map[ingredients.indexOf(item).toString()] to item
+        }
+        return map
+    }
+
+    fun updateChildren(nodeMap:HashMap<String,Any>){
+        dbRef.updateChildren(nodeMap, object: DatabaseReference.CompletionListener{ //I have no idea why this is. https://stackoverflow.com/questions/59337641/the-read-and-write-calls-of-firebase-database-not-working-on-android
+            override fun onComplete(error: DatabaseError?, ref: DatabaseReference) {
+                TODO("Lack of Android experience blocks me from writing a good error.")
+                //Toast.makeText(MainActivity, "Error occurred while inserting recipe", Toast.LENGTH_LONG).show()
+                //
+            }
+        }
+        )
+    }
+
+
+
+}
+
+fun main(args: Array<String>) {
+}
+
---
+++ app/src/test/java/com/example/a491bproject/FirebaseRecipeDAOTest.kt
@@ -0,0 +1,4 @@
+package com.example.a491bproject
+
+class FirebaseRecipeDAOTest {
+}
