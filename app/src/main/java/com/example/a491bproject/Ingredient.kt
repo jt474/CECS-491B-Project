5f0d37c31e5102c204998637571d663514c1b270
toanos <toan.ha1896@gmail.com>
2021-10-26 23:27:28 -0700

    MyIngredientActivity.kt: implement MyIngredients singleton class.

---
+++ app/src/main/java/com/example/a491bproject/Ingredient.kt
@@ -0,0 +1,3 @@
+package com.example.a491bproject
+
+class Ingredient (name: String, quantity: String = "1", unit: String = "")
--- app/src/main/java/com/example/a491bproject/MyIngredientsActivity.kt
+++ app/src/main/java/com/example/a491bproject/MyIngredientsActivity.kt
@@ -15,3 +15,20 @@ class MyIngredientsActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_back).setOnClickListener { this.finish() }
     }
 }
+
+object MyIngredients {
+    private val ingredients = mutableListOf<Ingredient>()
+
+    fun addIngredient(ingredient: Ingredient) {
+        ingredients.add(ingredient)
+    }
+
+    init {
+        ingredients.add(Ingredient("pineapple"))
+        ingredients.add(Ingredient("apple", "5"))
+        ingredients.add(Ingredient("banana"))
+        ingredients.add(Ingredient("egg", "10"))
+        ingredients.add(Ingredient("lettuce"))
+        ingredients.add(Ingredient("rib eye", "20", "oz"))
+    }
+}
