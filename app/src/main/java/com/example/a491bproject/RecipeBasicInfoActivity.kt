28e5053d2804d1bf4aaf942bf3120c13a72cfd21
Jace <jacen131@gmail.com>
2021-11-01 11:47:12 -0700

    Intial Commit

---
+++ app/src/main/java/com/example/a491bproject/RecipeBasicInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a491bproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeBasicInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_basic_info)
+    }
+}
