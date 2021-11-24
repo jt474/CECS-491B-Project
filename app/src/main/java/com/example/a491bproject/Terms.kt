ceb0bd5c68da5ea685681454ae62e2dd617eebff
jt474 <49823439+jt474@users.noreply.github.com>
2021-11-23 18:56:25 -0800

    Terms

---
+++ app/src/main/java/com/example/a491bproject/Terms.kt
@@ -0,0 +1,18 @@
+package com.example.a491bproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.TextView
+
+class Terms : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms)
+
+        val terms: TextView = findViewById<TextView>(R.id.terms)
+
+        terms.movementMethod = ScrollingMovementMethod();
+    }
+}
